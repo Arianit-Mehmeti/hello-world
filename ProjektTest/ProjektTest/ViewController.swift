ff1aaa74102dfcacb38487c2c2da4bb768dd6380
Arianit <arianitmehmeti4@gmail.com>
2015-09-16 10:50:53 +0200

    ProjektTest u shtua ne hello-world

---
+++ ProjektTest/ProjektTest/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ProjektTest
+//
+//  Created by MacBook Pro on 9/15/15.
+//  Copyright (c) 2015 AR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func logInTapped(sender: AnyObject) {
+    }
+
+}
+
